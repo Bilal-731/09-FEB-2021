99daed4acaf1b0e6be38a592c2a3c5b96c06b014
Muhammad Bilal Gulzar <79194902+Bilal-731@users.noreply.github.com>
2021-02-24 11:39:00 +0500

    Add files via upload

---
+++ first program (basics of c++).cpp
@@ -0,0 +1,26 @@
+#include<iostream> //pre-process or directive (adding basics of c++) 
+
+//<iostream> -- written with include, [header file (input-output stream)-(collection of predefined functions)]
+
+//cout -- consol output (to get the output)
+
+using namespace std; //std means standard, we use namesoaces in c++ language so here we are using std as a namespace(namespaces are already assingned in c++ and here by using std we mean that we are using standard namespaces)
+
+//int-keyword for integer
+
+int main() //we can run the program without int*if blank (other keywords to be used according to the prioritized output of the program)
+
+{
+	cout<<"Happy first day at C, AniPao";
+}
+
+
+
+/*
+< angled bracket
+() parentheses
+{} curly brackets
+# Operators
+consol the black window is called consol
+
+*/
